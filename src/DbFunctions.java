dc3358b21b697dcbc5f2c44d273e0bcdac0f2ca3
rukna01 <ankurjaiswal19999@gmail.com>
2023-01-29 19:47:34 +0530

    Added Database(postgres) functionally

--- src/DbFunctions.java
+++ src/DbFunctions.java
@@ -1,5 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class DbFunctions {
 
@@ -16,5 +18,114 @@ public class DbFunctions {
             System.out.print(e);
         }
         return con;
+    }
+    public void createTable(Connection conn,String table_name) {
+        Statement statement;  // this is the object
+        try {
+            String query = "create table " + table_name + "(empid SERIAL,name varchar(200),address varchar(200),primary key(empid));";
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Table created");
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public void insert_row(Connection conn,String table_name,String name,String address){
+        Statement statement;
+        try{
+            String query=String.format("insert into %s(name,address) values('%s','%s');",table_name,name,address);
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+
+            System.out.println("Row Inserted");
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void read_data(Connection conn ,String table_name){
+        Statement statement;
+        ResultSet rs =null;
+        try{
+            String query=String.format("select * from %s",table_name);
+            statement=conn.createStatement();
+            rs = statement.executeQuery(query);
+            while(rs.next()){
+                System.out.print(rs.getString("empid")+"\t");
+                System.out.print(rs.getString("name")+"\t");
+                System.out.println(rs.getString("address"));
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public void update_name(Connection conn,String table_name,String old_name,String new_name){
+        Statement statement;
+        try{
+            String query=String.format("update %s set name='%s' where name='%s'",table_name,new_name,old_name);
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Data Updated");
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public void update_name_byId(Connection conn,String table_name,int empid,String new_name){
+        Statement statement;
+        try{
+            String query=String.format("update %s set name='%s' where empid='%s'",table_name,new_name,empid);
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Data Updated");
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public void search_by_name(Connection conn,String table_name,String name){
+        Statement statement;
+        ResultSet rs=null;
+        try{
+            String query=String.format("select  * from %s where name ='%s'",table_name,name);
+            statement=conn.createStatement();
+            rs=statement.executeQuery(query);
+            while(rs.next()){
+                System.out.print(rs.getString("empid")+"\t");
+                System.out.print(rs.getString("name")+"\t");
+                System.out.println(rs.getString("address")+"\t");
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void delete_row_by_name(Connection conn,String table_name,String name){
+        Statement statement;
+        try{
+            String query=String.format("delete from %s where name ='%s'",table_name,name);
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Data deleted");
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public void delete_table(Connection conn,String table_name){
+        Statement statement;
+        try{
+            String query=String.format("drop table %s",table_name);
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Table deleted");
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,15 @@ public class Main {
     public static void main(String[] args) {
         DbFunctions db = new DbFunctions();
         Connection conn =db.connect_to_db("dump01","postgres","rootroot");
+        db.delete_table(conn,"employee ");
+        db.createTable(conn,"employee");
+        db.insert_row(conn,"employee","peter","India");
+//          db.read_data(conn,"employee");
+//          db.update_name(conn,"employee","raj","peter");
+//          db.update_name_byId(conn,"employee",1,"ollie");
+
+//          db.search_by_name(conn,"employee","peter");
+//          db.delete_row_by_name(conn,"employee","peter");
+        db.read_data(conn,"employee");
     }
 }
